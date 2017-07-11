4bc5f23333d62999a68b0b4f748ac0035c99aeae
OliviaAo <aoli.lady813@gmail.com>
2017-07-11 16:35:40 +1000

    second commit

--- Assignment/assignment1/Fuzzy-1.1/test/test_fuzzy.py
+++
@@ -1,9 +0,0 @@
-import ctypes
-
-import fuzzy
-
-def test_soundex_does_not_mutate_strings():
-	phrase = 'FancyFree'
-	fuzzy.Soundex(4)(phrase)
-	buffer = ctypes.c_char_p(phrase)
-	assert str(buffer) == "c_char_p('FancyFree')"
